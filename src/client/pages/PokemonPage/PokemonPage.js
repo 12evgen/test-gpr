bfc0638cfcfa6f45d0b0121c7483847f3eb10a20
evgen <1212evgen@gmail.com>
2020-01-25 17:11:13 +0600

    some changes in styles

--- src/client/pages/PokemonPage/PokemonPage.js
+++ src/client/pages/PokemonPage/PokemonPage.js
@@ -45,6 +45,9 @@ class PokemonPage extends Component {
   componentDidMount () {
     reaction(() => this.props.match.params.id, async (data) => {
       this.props.pokemonDetailsStore.getPokemonDetails(this.props.match.params.id)
+      this.setState(() => ({ loading: true }))
+    }, { fireImmediately: true })
+    reaction(() => this.props.pokemonDetailsStore.pokemonDetails, async (data) => {
       this.setState(() => ({ loading: false }))
     }, { fireImmediately: true })
   }
@@ -83,128 +86,130 @@ class PokemonPage extends Component {
         <Helmet>
           <title>{pokemonDetails.name} - Pokemon</title>
         </Helmet>
-        <div className='pokemon-detais'>
-          <div className='pokemon-detais__name'>
-            <div className='pokemon-detais__main'>
-              <div className='pokemon-detais__avatar'>
-                <img
-                  src={`https://pokeres.bastionbot.org/images/pokemon/${pokemonDetails.id}.png`}
-                  alt={pokemonDetails.name}
-                />
-              </div>
-              <div className='pokemon-detais__desc'>
-                <h1 className='pokemon-detais__title'>
-                  {`${pokemonDetails.name} #${pokemonDetails.id}`}
-                </h1>
-
-                {/* Desc */}
-                <div className='pokemon-detais__text'>
-                  {pokemonSpecies.flavor_text_entries[1].language.name === 'en'
-                    ? pokemonSpecies.flavor_text_entries[1].flavor_text
-                    : pokemonSpecies.flavor_text_entries[2].flavor_text}
+        <div className={`pokemon-detais pokemon-detais--${pokemonSpecies.color.name}`}>
+          <div className={`wrapper`}>
+            <div className='pokemon-detais__name'>
+              <div className='pokemon-detais__main'>
+                <div className='pokemon-detais__avatar'>
+                  <img
+                    src={`https://pokeres.bastionbot.org/images/pokemon/${pokemonDetails.id}.png`}
+                    alt={pokemonDetails.name}
+                  />
                 </div>
+                <div className='pokemon-detais__desc'>
+                  <h1 className='pokemon-detais__title'>
+                    {`${pokemonDetails.name} #${pokemonDetails.id}`}
+                  </h1>
 
-                {/* Specifications */}
-                <div className='pokemon-detais__specifications'>
-                  <h3 className='pokemon-detais__specifications-title'>Profile</h3>
-                  <div className='pokemon-detais__specifications-wrapper'>
-                    <div className='pokemon-detais__specifications-item'>
-                      <div className='pokemon-detais__specifications-name'>Height</div>
-                      <div className='pokemon-detais__specifications-value'>{`${pokemonDetails.height / 10} m`}</div>
-                    </div>
-                    <div className='pokemon-detais__specifications-item'>
-                      <div className='pokemon-detais__specifications-name'>Weight</div>
-                      <div className='pokemon-detais__specifications-value'>{`${pokemonDetails.weight / 10} kg`}</div>
-                    </div>
-                    <div className='pokemon-detais__specifications-item'>
-                      <div className='pokemon-detais__specifications-name'>Gender Rate</div>
-                      <div className='pokemon-detais__specifications-value'>
-                        {pokemonSpecies.gender_rate !== -1 ? (
-                          `${pokemonSpecies.gender_rate}%`
-                        ) : (
-                          <span>Gender Unknown</span>
-                        )}
+                  {/* Desc */}
+                  <div className='pokemon-detais__text'>
+                    {pokemonSpecies.flavor_text_entries[1].language.name === 'en'
+                      ? pokemonSpecies.flavor_text_entries[1].flavor_text
+                      : pokemonSpecies.flavor_text_entries[2].flavor_text}
+                  </div>
+
+                  {/* Specifications */}
+                  <div className='pokemon-detais__specifications'>
+                    <h3 className='pokemon-detais__specifications-title'>Profile</h3>
+                    <div className='pokemon-detais__specifications-wrapper'>
+                      <div className='pokemon-detais__specifications-item'>
+                        <div className='pokemon-detais__specifications-name'>Height</div>
+                        <div className='pokemon-detais__specifications-value'>{`${pokemonDetails.height / 10} m`}</div>
                       </div>
-                    </div>
-                    <div className='pokemon-detais__specifications-item'>
-                      <div className='pokemon-detais__specifications-name'>Capture Rate</div>
-                      <div className='pokemon-detais__specifications-value'>
-                        {`${pokemonSpecies.capture_rate}%`}
+                      <div className='pokemon-detais__specifications-item'>
+                        <div className='pokemon-detais__specifications-name'>Weight</div>
+                        <div className='pokemon-detais__specifications-value'>{`${pokemonDetails.weight / 10} kg`}</div>
                       </div>
-                    </div>
-                    <div className='pokemon-detais__specifications-item'>
-                      <div className='pokemon-detais__specifications-name'>Egg Groups:</div>
-                      <div className='pokemon-detais__specifications-value'>
-                        {pokemonSpecies.egg_groups[0].name}
+                      <div className='pokemon-detais__specifications-item'>
+                        <div className='pokemon-detais__specifications-name'>Gender Rate</div>
+                        <div className='pokemon-detais__specifications-value'>
+                          {pokemonSpecies.gender_rate !== -1 ? (
+                            `${pokemonSpecies.gender_rate}%`
+                          ) : (
+                            <span>Gender Unknown</span>
+                          )}
+                        </div>
+                      </div>
+                      <div className='pokemon-detais__specifications-item'>
+                        <div className='pokemon-detais__specifications-name'>Capture Rate</div>
+                        <div className='pokemon-detais__specifications-value'>
+                          {`${pokemonSpecies.capture_rate}%`}
+                        </div>
+                      </div>
+                      <div className='pokemon-detais__specifications-item'>
+                        <div className='pokemon-detais__specifications-name'>Egg Groups:</div>
+                        <div className='pokemon-detais__specifications-value'>
+                          {pokemonSpecies.egg_groups[0].name}
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                {/* Types */}
-                <div className='pokemon-detais__types'>
-                  <h3 className='pokemon-detais__types-title'>Type</h3>
-                  <div className='pokemon-detais__types-wrapper'>
-                    {pokemonDetails.types.map(type => {
-                      return (
-                        <Tag key={type.slot} color={this.colorType[type.slot]}>{type.type.name}</Tag>
-                      )
-                    })}
+                  {/* Types */}
+                  <div className='pokemon-detais__types'>
+                    <h3 className='pokemon-detais__types-title'>Type</h3>
+                    <div className='pokemon-detais__types-wrapper'>
+                      {pokemonDetails.types.map(type => {
+                        return (
+                          <Tag key={type.slot} color={this.colorType[type.slot]}>{type.type.name}</Tag>
+                        )
+                      })}
+                    </div>
                   </div>
-                </div>
 
-                {/* Abilities */}
-                <div className='pokemon-detais__abilities'>
-                  <h3 className='pokemon-detais__abilities-title'>Abilities</h3>
-                  <div className='pokemon-detais__abilities-wrapper'>
-                    {pokemonDetails.abilities.map(ability => {
-                      return (
-                        <Tag key={ability.slot} color={this.colorAbilities[ability.slot]}>{ability.ability.name}</Tag>
-                      )
-                    })}
+                  {/* Abilities */}
+                  <div className='pokemon-detais__abilities'>
+                    <h3 className='pokemon-detais__abilities-title'>Abilities</h3>
+                    <div className='pokemon-detais__abilities-wrapper'>
+                      {pokemonDetails.abilities.map(ability => {
+                        return (
+                          <Tag key={ability.slot} color={this.colorAbilities[ability.slot]}>{ability.ability.name}</Tag>
+                        )
+                      })}
+                    </div>
                   </div>
-                </div>
 
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className='pokemon-detais__evolution'>
-            <h3 className='pokemon-detais__evolution-title'>Evolution</h3>
-            <div className='pokemon-detais__evolution-wrapper'>
-              {this.evolution.map(gen => {
-                const { name, url } = gen.species
-                return (
-                  <div className='pokemon-detais__evolution-item' key={name}>
-                    <img
-                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${parseId(url)}.png`}
-                      className='pokemon-detais__evolution-avatar'
-                      alt={name}
-                    />
-                    <Link to={`/pokemon/${parseId(url)}`} className='pokemon-detais__evolution-link'>
-                      {name}
-                    </Link>
-                  </div>
-                )
-              })}
+            <div className='pokemon-detais__evolution'>
+              <h3 className='pokemon-detais__evolution-title'>Evolution</h3>
+              <div className='pokemon-detais__evolution-wrapper'>
+                {this.evolution.map(gen => {
+                  const { name, url } = gen.species
+                  return (
+                    <div className='pokemon-detais__evolution-item' key={name}>
+                      <img
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${parseId(url)}.png`}
+                        className='pokemon-detais__evolution-avatar'
+                        alt={name}
+                      />
+                      <Link to={`/pokemon/${parseId(url)}`} className='pokemon-detais__evolution-link'>
+                        {name}
+                      </Link>
+                    </div>
+                  )
+                })}
+              </div>
             </div>
-          </div>
 
-          <div className='pokemon-detais__stats'>
-            <h3 className='pokemon-detais__stats-title'>Statistics</h3>
-            <div className='pokemon-detais__stats-wrapper'>
-              {pokemonDetails.stats.map(stat => {
-                return (
-                  <div className='pokemon-detais__stats-item' key={stat.stat.name}>
-                    <div className='pokemon-detais__stats-name'>
-                      {stat.stat.name}
+            <div className='pokemon-detais__stats'>
+              <h3 className='pokemon-detais__stats-title'>Statistics</h3>
+              <div className='pokemon-detais__stats-wrapper'>
+                {pokemonDetails.stats.map(stat => {
+                  return (
+                    <div className='pokemon-detais__stats-item' key={stat.stat.name}>
+                      <div className='pokemon-detais__stats-name'>
+                        {stat.stat.name}
+                      </div>
+                      <div className='pokemon-detais__stats-progress'>
+                        <Progress percent={stat.base_stat} />
+                      </div>
                     </div>
-                    <div className='pokemon-detais__stats-progress'>
-                      <Progress percent={stat.base_stat} />
-                    </div>
-                  </div>
-                )
-              })}
+                  )
+                })}
+              </div>
             </div>
           </div>
         </div>
